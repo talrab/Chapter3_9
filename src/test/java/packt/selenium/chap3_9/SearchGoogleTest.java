76b956fe5b65495f86a08408af32f18809428aac
chmod <talr@TALR1-TP.haifa.ibm.com>
2016-11-09 09:59:22 +0200

    adding everything

---
+++ src/test/java/packt/selenium/chap3_9/SearchGoogleTest.java
@@ -0,0 +1,41 @@
+package packt.selenium.chap3_9;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import packt.selenium.chap3_9.pageobjects.Google;
+import packt.selenium.chap3_9.pageobjects.GoogleSearchPage;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Ripon on 11/19/2015.
+ */
+public class SearchGoogleTest {
+    private WebDriver driver;
+    public Google googlePage;
+    public GoogleSearchPage searchPage;
+
+    @Before
+    public void setUp() throws Exception {
+        //driver = new FirefoxDriver();
+        System.setProperty("webdriver.chrome.driver", "c:\\D\\Online Courses\\Selenium and continoues integration\\Tools\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+    @Test
+    public void testGoogleSearch(){
+        googlePage = new Google(driver);
+        searchPage = googlePage.goToSearchPage();
+        searchPage.getNumberOfResults();
+    }
+}
